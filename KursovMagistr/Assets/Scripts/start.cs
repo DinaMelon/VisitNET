bf7f79f4d6944b32444ae24929007f4b6d6bcf09
DinaDeynego <alphalegend44@gmail.com>
2026-04-27 20:18:42 +0300

    Update start.cs

--- KursovMagistr/Assets/Scripts/start.cs
+++ KursovMagistr/Assets/Scripts/start.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class start : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //загружаем сцену с экскурсией
     public void StartGame()
     {
         SceneManager.LoadScene("SampleScene");
